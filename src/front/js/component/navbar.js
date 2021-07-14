49b48b7296d51f372618839181d98f143e2c57ac
Alejandro Ariel Moran <alejandroariel.moran9@gmail.com>
2021-07-14 13:42:42 +0000

    starwars homework

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,30 +3,52 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">
-					{" "}
-					<img
-						className="star"
-						src="https://imagenesparapeques.com/wp-content/uploads/2016/01/Star-Wars-Logo.png"
-					/>
-				</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-					<button
-						id="btnGroupDrop1"
-						type="button"
-						className="btn btn-secondary dropdown-toggle"
-						data-toggle="dropdown"
-						aria-haspopup="true"
-						aria-expanded="false">
-						<i className="fas fa-cog" />
-					</button>
-				</Link>
+		<nav className="navbar navbar-expand-lg navbar-light bg-light">
+			<div className="container">
+				<img
+					className="star"
+					src="https://imagenesparapeques.com/wp-content/uploads/2016/01/Star-Wars-Logo.png"
+				/>
+
+				<form className="form-inline ml-5">
+					<div className="btn-group">
+						<button
+							type="button"
+							className="btn btn-primary dropdown-toggle"
+							data-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false">
+							Favorites
+						</button>
+					</div>
+				</form>
 			</div>
 		</nav>
+
+		//<nav className="navbar navbar-light bg-light mb-3">
+		// 	<Link to="/">
+		// 		<span className="navbar-brand mb-0 h1">
+		// 			{" "}
+		// 			<img
+		// 				className="star"
+		// 				src="https://imagenesparapeques.com/wp-content/uploads/2016/01/Star-Wars-Logo.png"
+		// 			/>
+		// 		</span>
+		// 	</Link>
+		// 	<div className="ml-auto">
+		// 		<Link to="/demo">
+		// 			<button className="btn btn-primary">Check the Context in action</button>
+		// 			<button
+		// 				id="btnGroupDrop1"
+		// 				type="button"
+		// 				className="btn btn-secondary dropdown-toggle"
+		// 				data-toggle="dropdown"
+		// 				aria-haspopup="true"
+		// 				aria-expanded="false">
+		// 				<i className="fas fa-cog" />
+		// 			</button>
+		// 		</Link>
+		// 	</div>
+		// </nav>
 	);
 };
